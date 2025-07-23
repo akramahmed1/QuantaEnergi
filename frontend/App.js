21a13064db4f5b930f9b42f0912cf4b143aacc91
akramahmed1 <97985065+akramahmed1@users.noreply.github.com>
2025-07-23 04:10:34 -0500

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,5 @@
 import { MantineProvider } from '@mantine/core';
 function App() {
-  return <MantineProvider><h1>EnergyOpti AI</h1></MantineProvider>;
+  return <MantineProvider><h1>EnergyOpti Platform</h1></MantineProvider>;
 }
 export default App;
